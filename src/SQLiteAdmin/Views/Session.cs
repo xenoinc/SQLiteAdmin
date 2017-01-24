757dc0bb4283f3e775727a6b34242184df3b51d0
Damian Suess <Damian.Suess@omnicell.com>
2017-01-24 18:34:27 -0500

    Initial commit

---
+++ src/SQLiteAdmin/Views/Session.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SQLiteAdmin.Views
+{
+  public partial class Session : Form
+  {
+    public Session()
+    {
+      InitializeComponent();
+    }
+
+    private void Session_Load(object sender, EventArgs e)
+    {
+
+    }
+  }
+}
