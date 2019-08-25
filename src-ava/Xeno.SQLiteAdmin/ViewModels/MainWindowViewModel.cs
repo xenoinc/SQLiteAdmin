da080b9e511619cbabc71f7efec5b8774385385b
Damian Suess <dsuess@xenoinc.org>
2019-08-25 15:17:08 -0400

    Added Avalonia project space to prototype a cross-platform SQLite admin tool

---
+++ src-ava/Xeno.SQLiteAdmin/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿/* Copyright Xeno Innovations, Inc. 2019
+ * Date:    2019-8-25
+ * Author:  Damian Suess
+ * File:    MainWindowViewModel.cs
+ * Description:
+ *
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xeno.SQLiteAdmin.ViewModels
+{
+  public class MainWindowViewModel : ViewModelBase
+  {
+    public string Greeting => "Welcome to Avalonia!";
+  }
+}
---
+++ src-ava/Xeno.SQLiteAdmin/ViewModels/ViewModelBase.cs
@@ -0,0 +1,19 @@
+﻿/* Copyright Xeno Innovations, Inc. 2019
+ * Date:    2019-8-25
+ * Author:  Damian Suess
+ * File:    ViewModelBase.cs
+ * Description:
+ *
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace Xeno.SQLiteAdmin.ViewModels
+{
+  public class ViewModelBase : ReactiveObject
+  {
+  }
+}
