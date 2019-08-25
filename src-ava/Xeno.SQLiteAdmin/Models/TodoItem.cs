3244f998b48d26230cc7a41f7c2496537b64976a
Damian Suess <dsuess@xenoinc.org>
2019-08-25 15:30:20 -0400

    Updated Visual Studio editor rule sets w/ standard Xeno Innovations coding style rules

--- src-ava/Xeno.SQLiteAdmin/Models/TodoItem.cs
+++ src-ava/Xeno.SQLiteAdmin/Models/TodoItem.cs
@@ -1,13 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿/* Copyright Xeno Innovations, Inc. 2019
+ * Date:    2019-8-25
+ * Author:  Damian Suess
+ * File:    TodoItem.cs
+ * Description:
+ *
+ */
 
 namespace Xeno.SQLiteAdmin.Models
 {
   public class TodoItem
   {
     public string Description { get; set; }
-    
+
     public bool IsChecked { get; set; }
   }
 }
