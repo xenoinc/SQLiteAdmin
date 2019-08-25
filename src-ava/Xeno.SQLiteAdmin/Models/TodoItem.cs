374722c0b373c643ed24d8c63c4358187a4d1007
Damian Suess <dsuess@xenoinc.org>
2019-08-25 15:19:54 -0400

    Added stylecop rulesets

---
+++ src-ava/Xeno.SQLiteAdmin/Models/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xeno.SQLiteAdmin.Models
+{
+  public class TodoItem
+  {
+    public string Description { get; set; }
+    
+    public bool IsChecked { get; set; }
+  }
+}
